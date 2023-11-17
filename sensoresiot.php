3e6336e0aa953f5a327a0d797486a9da2096300b
JonathasLuan <112669858+JonathasLuan@users.noreply.github.com>
2023-11-17 20:08:01 -0300

    Update sensoresiot.php

--- sensoresiot.php
+++ sensoresiot.php
@@ -35,149 +35,163 @@ session_start();
       <h1>Sensores na IoT: Capturando o Mundo em Dados Conectados</h1>
       <img style="width: 100%;" src="img/.jpg" alt="">
       <br><br>
-      A Internet das Coisas (IoT) depende crucialmente de sensores para coletar dados do mundo físico e transformá-los
+      <p>A Internet das Coisas (IoT) depende crucialmente de sensores para coletar dados do mundo físico e transformá-los
       em informações significativas. Estes dispositivos sensíveis são a espinha dorsal da IoT, permitindo desde a
       monitorização ambiental até a automação inteligente. Neste artigo, exploraremos os tipos comuns de sensores
-      utilizados na IoT, destacando suas aplicações, desde a medição de temperatura até a detecção de movimento.
+      utilizados na IoT, destacando suas aplicações, desde a medição de temperatura até a detecção de movimento.</p>
+<ol>
+        <li><h2>Sensores de Temperatura:</h2></li>
 
-      **1. Sensores de Temperatura:**
+      <h3>Aplicações:</h3>
+  <ul>
+      <li><b>Climatização Residencial e Comercial:</b> Permitem ajustar automaticamente a temperatura com base nas condições
+      ambientais.</li>
+      <li><b>Monitoramento de Equipamentos Sensíveis:</b> Garantem que dispositivos eletrônicos e servidores operem dentro de
+      faixas térmicas seguras.</li>
+</ul>
+  
+      <li><h2>Sensores de Umidade:</h2></li>
 
-      *Aplicações:*
-      - **Climatização Residencial e Comercial:** Permitem ajustar automaticamente a temperatura com base nas condições
-      ambientais.
-      - **Monitoramento de Equipamentos Sensíveis:** Garantem que dispositivos eletrônicos e servidores operem dentro de
-      faixas térmicas seguras.
-
-      **2. Sensores de Umidade:**
-
-      *Aplicações:*
-      - **Agricultura Inteligente:** Monitoram a umidade do solo para otimizar a irrigação.
-      - **Controle de Ambiente em Indústrias Farmacêuticas:** Garantem que as condições de armazenamento estejam dentro
+      <h3>Aplicações:</h3>
+  <ul>
+      <li><b>Agricultura Inteligente:</b> Monitoram a umidade do solo para otimizar a irrigação.
+      <li><b>Controle de Ambiente em Indústrias Farmacêuticas:</b> Garantem que as condições de armazenamento estejam dentro
       de limites aceitáveis.
+</ul>
+      <li><h2>Sensores de Luz (Fotossensores):</h2></li>
 
-      **3. Sensores de Luz (Fotossensores):**
+      <h3>Aplicações:</h3>
+  <ul>
+      <li><b>Iluminação Pública Inteligente:</b> Ajustam a intensidade da luz com base nas condições de luminosidade natural.</li>
+      <li><b>Economia de Energia em Edifícios:</b> Regulam a iluminação interna de acordo com a luz externa disponível.</li>
+</ul>
+      <li>Sensores de Movimento (PIR - Passive Infrared):</h2></li>
 
-      *Aplicações:*
-      - **Iluminação Pública Inteligente:** Ajustam a intensidade da luz com base nas condições de luminosidade natural.
-      - **Economia de Energia em Edifícios:** Regulam a iluminação interna de acordo com a luz externa disponível.
+      <h3>Aplicações:</h3>
+  <ul>
+      <li><b>Segurança Residencial e Empresarial:</b> Disparam alarmes ou notificações quando movimento é detectado.</li>
+      <li><b>Iluminação de Ambientes:</b> Ativam luzes automaticamente quando uma presença é detectada.</li>
+</ul>
+      <li><h2>Sensores de Pressão:</h2></li>
 
-      **4. Sensores de Movimento (PIR - Passive Infrared):**
+      <h3>Aplicações:</h3>
+  <ul>
+      <li><b>Monitoramento de Pneus em Veículos:</b> Verificam a pressão dos pneus e alertam sobre possíveis vazamentos.</li>
+      <li><b>Monitoramento de Níveis de Líquidos em Indústrias:</b> Medem a pressão em tanques para controlar o fluxo de
+      líquidos.</li>
+</ul>
+      <li><h2>Sensores de Gás:</h2></li>
 
-      *Aplicações:*
-      - **Segurança Residencial e Empresarial:** Disparam alarmes ou notificações quando movimento é detectado.
-      - **Iluminação de Ambientes:** Ativam luzes automaticamente quando uma presença é detectada.
+      <h3>Aplicações:</h3>
+  <ul>
+      <li><b>Detecção de Vazamentos em Ambientes Residenciais:</b> Detectam a presença de gases nocivos, como monóxido de
+      carbono.</li>
+      <li><b>Controle de Qualidade do Ar em Ambientes Industriais:</b> Monitoram a concentração de gases em fábricas e
+      instalações industriais.</li>
+</ul>
+      <li><h2>Sensores de Som (Microfones):</h2></li>
 
-      **5. Sensores de Pressão:**
+      <h3>Aplicações:</h3>
+<ul>
+      <li><b>Segurança Residencial e Empresarial:</b> Detectam ruídos suspeitos e acionam alarmes.</li>
+      <li><b>Saúde Pública:</b> Monitoram níveis de poluição sonora em áreas urbanas.</li>
+</ul>
+      <li><h2>Sensores de Vibração:</h2></li>
 
-      *Aplicações:*
-      - **Monitoramento de Pneus em Veículos:** Verificam a pressão dos pneus e alertam sobre possíveis vazamentos.
-      - **Monitoramento de Níveis de Líquidos em Indústrias:** Medem a pressão em tanques para controlar o fluxo de
-      líquidos.
+      <h3>Aplicações:</h3>
+<ul>
+      <li><b>Manutenção Preditiva em Máquinas Industriais:</b> Identificam padrões de vibração indicativos de desgaste ou
+      falhas.</li>
+      <li><b>Segurança Estrutural em Edifícios:</b> Monitoram a vibração para identificar potenciais problemas de
+      estabilidade.</li>
+</ul>
+      <li><h2>Sensores de Proximidade (Ultrassônicos, Infravermelhos):</h2></li>
 
-      **6. Sensores de Gás:**
+      <h3>Aplicações:</h3>
+        <ul>
+      <li><b>Tecnologia de Tela Sensível ao Toque:</b> Detectam a proximidade de dedos para interação.</li>
+      <li><b>Automação Residencial:</b> Ativam dispositivos quando detectam a presença próxima.</li>
+</ul>
+      <li><h2>Sensores de GPS:</h2></li>
 
-      *Aplicações:*
-      - **Detecção de Vazamentos em Ambientes Residenciais:** Detectam a presença de gases nocivos, como monóxido de
-      carbono.
-      - **Controle de Qualidade do Ar em Ambientes Industriais:** Monitoram a concentração de gases em fábricas e
-      instalações industriais.
+      <h3>Aplicações:</h3>
+        <ul>
+      <li><b>Navegação Veicular:</b> Fornecem informações de localização em tempo real para sistemas de navegação.</li>
+      <li><b>Logística e Rastreamento de Ativos:</b> Permitem o rastreamento preciso de mercadorias em trânsito.</li>
+        </ul>
+</ol>
 
-      **7. Sensores de Som (Microfones):**
+      <h2>Considerações:</h2>
 
-      *Aplicações:*
-      - **Segurança Residencial e Empresarial:** Detectam ruídos suspeitos e acionam alarmes.
-      - **Saúde Pública:** Monitoram níveis de poluição sonora em áreas urbanas.
-
-      **8. Sensores de Vibração:**
-
-      *Aplicações:*
-      - **Manutenção Preditiva em Máquinas Industriais:** Identificam padrões de vibração indicativos de desgaste ou
-      falhas.
-      - **Segurança Estrutural em Edifícios:** Monitoram a vibração para identificar potenciais problemas de
-      estabilidade.
-
-      **9. Sensores de Proximidade (Ultrassônicos, Infravermelhos):**
-
-      *Aplicações:*
-      - **Tecnologia de Tela Sensível ao Toque:** Detectam a proximidade de dedos para interação.
-      - **Automação Residencial:** Ativam dispositivos quando detectam a presença próxima.
-
-      **10. Sensores de GPS:**
-
-      *Aplicações:*
-      - **Navegação Veicular:** Fornecem informações de localização em tempo real para sistemas de navegação.
-      - **Logística e Rastreamento de Ativos:** Permitem o rastreamento preciso de mercadorias em trânsito.
-
-      **Considerações Finais:**
-
-      A diversidade de sensores na IoT desempenha um papel crucial na coleta de dados precisos, possibilitando uma gama
+      <p>A diversidade de sensores na IoT desempenha um papel crucial na coleta de dados precisos, possibilitando uma gama
       vasta de aplicações. À medida que a tecnologia avança, novos tipos de sensores e aprimoramentos contínuos
       contribuirão para a evolução constante da Internet das Coisas, transformando a forma como interagimos com o mundo
-      ao nosso redor.
+      ao nosso redor.</p>
 
 
-      **Como Funciona um Sensor: Transformando o Físico em Dados Digitais**
+      <h2>Como Funciona um Sensor: Transformando o Físico em Dados Digitais</h2>
 
-      Os sensores desempenham um papel vital na captura de informações do mundo físico e sua transformação em dados
+      <p>Os sensores desempenham um papel vital na captura de informações do mundo físico e sua transformação em dados
       digitais utilizáveis. Esses dispositivos sensíveis são fundamentais para a operação de sistemas na Internet das
       Coisas (IoT), automação industrial, dispositivos eletrônicos e muito mais. Vamos explorar o funcionamento básico
-      de um sensor e como ele converte fenômenos físicos em dados que podemos entender e utilizar.
+      de um sensor e como ele converte fenômenos físicos em dados que podemos entender e utilizar.</p>
+<ol>
+      <li><h2>Princípio de Funcionamento:</h2></li>
 
-      **1. Princípio de Funcionamento:**
-
-      Os sensores operam com base em princípios físicos específicos relacionados à grandeza que estão destinados a
+      <p>Os sensores operam com base em princípios físicos específicos relacionados à grandeza que estão destinados a
       medir. Cada tipo de sensor é projetado para detectar um tipo particular de estímulo, como luz, temperatura,
       pressão, entre outros. O princípio subjacente a cada sensor determina como ele responderá às mudanças no ambiente
-      ao seu redor.
+      ao seu redor.</p>
 
-      **2. Captura da Grandeza Física:**
+      <li><h2>Captura da Grandeza Física:</h2></li>
 
-      O primeiro passo no funcionamento de um sensor é a captura da grandeza física alvo. Por exemplo, em sensores de
+      <p>O primeiro passo no funcionamento de um sensor é a captura da grandeza física alvo. Por exemplo, em sensores de
       temperatura, um termopar pode ser usado para medir variações de temperatura. Em um sensor de luz, uma fotocélula
-      ou fotodiodo pode detectar a intensidade luminosa.
+      ou fotodiodo pode detectar a intensidade luminosa.</p>
 
-      **3. Transdução:**
+      <li><h2>Transdução:</h2></li>
 
-      Após a captura da grandeza física, o sensor passa pelo processo de transdução, que é a conversão dessa grandeza em
+      <p>Após a captura da grandeza física, o sensor passa pelo processo de transdução, que é a conversão dessa grandeza em
       um sinal elétrico. Este é um ponto crucial, pois a maioria dos sistemas eletrônicos e digitais opera com sinais
-      elétricos. Existem vários métodos de transdução, dependendo do tipo de sensor:
+      elétricos. Existem vários métodos de transdução, dependendo do tipo de sensor:</p>
+<ul>
+      <li><b>Sensores Resistivos:</b> Alteram sua resistência elétrica em resposta a mudanças na grandeza física.</li>
+      <li><b>Sensores Capacitivos:</b> Modificam a capacitância em resposta a alterações no ambiente.</li>
+      <li><b>Sensores Piezoelétricos:</b> Geram uma carga elétrica em resposta a deformações mecânicas.</li>
+</ul>
+      <li><h2>Amplificação e Condicionamento do Sinal:</h2></li>
 
-      - **Sensores Resistivos:** Alteram sua resistência elétrica em resposta a mudanças na grandeza física.
-      - **Sensores Capacitivos:** Modificam a capacitância em resposta a alterações no ambiente.
-      - **Sensores Piezoelétricos:** Geram uma carga elétrica em resposta a deformações mecânicas.
-
-      **4. Amplificação e Condicionamento do Sinal:**
-
-      O sinal elétrico gerado pelo sensor pode ser fraco, então muitas vezes é necessário amplificá-lo para torná-lo
+      <p>O sinal elétrico gerado pelo sensor pode ser fraco, então muitas vezes é necessário amplificá-lo para torná-lo
       mais forte e mais facilmente processável. Além disso, o sinal pode precisar ser condicionado para remover ruídos
-      indesejados ou ajustado para garantir precisão na medição.
+      indesejados ou ajustado para garantir precisão na medição.</p>
 
-      **5. Conversão Analógico-Digital (ADC):**
+      <li><h2>Conversão Analógico-Digital (ADC):</h2></li>
 
-      Em muitos casos, especialmente na IoT e eletrônica digital, o sinal analógico amplificado precisa ser convertido
+      <p>Em muitos casos, especialmente na IoT e eletrônica digital, o sinal analógico amplificado precisa ser convertido
       em formato digital. Isso é feito por meio de um conversor analógico-digital (ADC). A conversão analógico-digital
       permite que os dados sejam representados digitalmente em bits, tornando-os adequados para processamento digital e
-      comunicação.
+      comunicação.</p>
 
-      **6. Processamento e Transmissão:**
+      <li><h2>6. Processamento e Transmissão</h2></li>
 
-      Os dados digitais resultantes podem ser processados internamente pelo próprio sensor ou transmitidos para outro
+      <p>Os dados digitais resultantes podem ser processados internamente pelo próprio sensor ou transmitidos para outro
       dispositivo para processamento adicional. Em muitos cenários IoT, os sensores transmitem dados para um hub central
-      ou uma nuvem, onde são processados, analisados e podem acionar ações específicas.
+      ou uma nuvem, onde são processados, analisados e podem acionar ações específicas.</p>
 
-      **7. Interpretação e Utilização:**
+      <li><h2>Interpretação e Utilização:</h2></li>
 
-      Finalmente, os dados processados são interpretados e utilizados para tomar decisões ou acionar dispositivos
+      <p>Finalmente, os dados processados são interpretados e utilizados para tomar decisões ou acionar dispositivos
       conectados. Em um termostato inteligente, por exemplo, a interpretação de dados de sensores de temperatura pode
-      resultar no ajuste automático da temperatura ambiente.
+      resultar no ajuste automático da temperatura ambiente.</p>
+</ol>
 
-      **Considerações Finais:**
+      <h2>Considerações Finais:</h2>
 
-      O funcionamento de um sensor é uma interação complexa entre princípios físicos, eletrônicos e digitais. Cada
+      <p>O funcionamento de um sensor é uma interação complexa entre princípios físicos, eletrônicos e digitais. Cada
       sensor é projetado para uma finalidade específica, e a escolha do tipo de sensor depende das características da
       grandeza física que se deseja medir. O desenvolvimento contínuo desses dispositivos contribui significativamente
       para avanços na IoT, automação e diversas outras áreas, proporcionando uma compreensão mais profunda e eficiente
-      do mundo ao nosso redor.
+      do mundo ao nosso redor.</p>
 
       <br><br>
       <hr>
